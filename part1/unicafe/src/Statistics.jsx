1fe2e3eb39640e256c286ded1b0455046e8df26f
vladibo13 <vladibo13@gmail.com>
2025-08-19 17:54:34 +0300

    unicafe step 2

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const score = good * 1 + neutral * 0 + bad * -1
+  const average = score / all
+  const positive = (good / all) * 100
+//   const positive = 
   return (
     <div>
         <h5>Statistics</h5>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive}</p>
     </div>
   )
 }
