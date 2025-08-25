62f7ba347518d8f8bd573b598b03683d4dde2814
vladibo13 <vladibo13@gmail.com>
2025-08-25 16:08:09 +0300

    3.4: Phonebook backend step 4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -43,6 +43,13 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
 app.get('/api/info', (req, res) => {
   const date = new Date()
   res.send(
