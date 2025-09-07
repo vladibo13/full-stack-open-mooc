5ec46792027ac3275ad9ebafc6ea060357d29815
vladibo13 <vladibo13@gmail.com>
2025-09-07 13:34:30 +0300

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogsList) => {
+    return blogsList.reduce((acc, blog) => acc + blog.likes , 0)
+}
+
+const favoriteBlog = (blogList) => {
+    if(!blogList.length) return null
+
+    return blogList.reduce((initialBlog, currentBlog) => initialBlog.likes > currentBlog.likes ? initialBlog : currentBlog)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
