e2024c7f306c2215fe1ced7d9b1954aa84ec2283
vladibo13 <vladibo13@gmail.com>
2025-09-12 10:53:10 +0300

    4.16*: Blog List Expansion, step 4

--- part4/blog/models/users.js
+++ part4/blog/models/users.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  name: String,
+  username: {
+    type: String,
+    required: true,
+    minLength: 3,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+    minLength: 3
+  },
   passwordHash: String,
   blogs: [
     {
