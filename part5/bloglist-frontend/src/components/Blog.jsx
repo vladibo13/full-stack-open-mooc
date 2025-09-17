e0a7fa7e3559dc7e3ee721eb6a82acb0dedb5237
vladibo13 <vladibo13@gmail.com>
2025-09-17 13:06:25 +0300

    5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog, user }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+import Togglable from "./Togglable"
+
+const Blog = ({ blog, user }) => {
+  const [view, setView] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const handleView = () => setView(!view)
+
+  return (
+      <div style={blogStyle}>
+        <span>{blog.title} - {blog.author}</span>
+        {!view ? (
+          <button onClick={handleView}>view</button>
+        )
+         :(
+          <div>  
+            <ul>
+              <li>url: {blog.title}</li>
+              <li>likes: {blog.likes}</li>
+            </ul>
+            <button onClick={handleView}>hide</button>
+          </div>  
+         )}
+        
+
+      </div> 
+  )
+}
+  
+ 
+
 
 export default Blog
