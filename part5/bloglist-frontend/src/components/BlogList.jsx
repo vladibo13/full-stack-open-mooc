c04b553910e85e6d269d9b9a0bab09cf0b0f4559
vladibo13 <vladibo13@gmail.com>
2025-09-19 11:06:06 +0300

    5.10: Blog List Frontend, step 10

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -4,7 +4,10 @@ import Blog from './Blog'
 const BlogList = ({blogs, handleLike}) => {
   return (
     <div>
-     {blogs.map(blog => <Blog key={blog.id} blog={blog} handleLike={handleLike} />)}
+     {blogs
+     .slice()
+     .sort((a, b) => b.likes - a.likes)
+     .map(blog => <Blog key={blog.id} blog={blog} handleLike={handleLike} />)}
     </div>
   )
 }
